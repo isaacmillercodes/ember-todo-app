57d2fa763dd2c40eaebcaafd32ed4e3e674a7f36
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-05-02 11:24:17 -0600

    added dist files to docs for gh-pages deployment

---
+++ docs/assets/tests.js
@@ -0,0 +1,541 @@
+'use strict';
+
+define('todo/tests/adapters/application.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - adapters/application.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'adapters/application.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/app.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - app.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/controllers/todos.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - controllers/todos.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/todos.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/controllers/todos/edit.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - controllers/todos/edit.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/todos/edit.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/controllers/todos/new.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - controllers/todos/new.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/todos/new.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/helpers/create-offline-ref', ['exports', 'firebase'], function (exports, _firebase) {
+  exports['default'] = createOfflineRef;
+
+  /**
+   * Creates an offline firebase reference with optional initial data and url.
+   *
+   * Be sure to `stubfirebase()` and `unstubfirebase()` in your tests!
+   *
+   * @param  {!Object} [initialData]
+   * @param  {string} [url]
+   * @param  {string} [apiKey]
+   * @return {!firebase.database.Reference}
+   */
+
+  function createOfflineRef(initialData) {
+    var url = arguments.length <= 1 || arguments[1] === undefined ? 'https://emberfire-tests-2c814.firebaseio.com' : arguments[1];
+    var apiKey = arguments.length <= 2 || arguments[2] === undefined ? 'AIzaSyC9-ndBb1WR05rRF1msVQDV6EBqB752m6o' : arguments[2];
+
+    if (!_firebase['default']._unStub) {
+      throw new Error('Please use stubFirebase() before calling this method');
+    }
+
+    var config = {
+      apiKey: apiKey,
+      authDomain: 'emberfire-tests-2c814.firebaseapp.com',
+      databaseURL: url,
+      storageBucket: ''
+    };
+
+    var app = undefined;
+
+    try {
+      app = _firebase['default'].app();
+    } catch (e) {
+      app = _firebase['default'].initializeApp(config);
+    }
+
+    var ref = app.database().ref();
+
+    app.database().goOffline(); // must be called after the ref is created
+
+    if (initialData) {
+      ref.set(initialData);
+    }
+
+    return ref;
+  }
+});
+define('todo/tests/helpers/destroy-app', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = destroyApp;
+
+  function destroyApp(application) {
+    _ember['default'].run(application, 'destroy');
+  }
+});
+define('todo/tests/helpers/destroy-app.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - helpers/destroy-app.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/destroy-app.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/helpers/destroy-firebase-apps', ['exports', 'ember', 'firebase'], function (exports, _ember, _firebase) {
+  exports['default'] = destroyFirebaseApps;
+  var run = _ember['default'].run;
+
+  /**
+   * Destroy all Firebase apps.
+   */
+
+  function destroyFirebaseApps() {
+    var deletions = _firebase['default'].apps.map(function (app) {
+      return app['delete']();
+    });
+    _ember['default'].RSVP.all(deletions).then(function () {
+      return run(function () {
+        // NOOP to delay run loop until the apps are destroyed
+      });
+    });
+  }
+});
+define('todo/tests/helpers/format-date.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - helpers/format-date.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'helpers/format-date.js should pass ESLint.\n4:10  - \'moment\' is not defined. (no-undef)');
+  });
+});
+define('todo/tests/helpers/format-markdown.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - helpers/format-markdown.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'helpers/format-markdown.js should pass ESLint.\n4:36  - \'showdown\' is not defined. (no-undef)');
+  });
+});
+define('todo/tests/helpers/module-for-acceptance', ['exports', 'qunit', 'ember', 'todo/tests/helpers/start-app', 'todo/tests/helpers/destroy-app'], function (exports, _qunit, _ember, _todoTestsHelpersStartApp, _todoTestsHelpersDestroyApp) {
+  var Promise = _ember['default'].RSVP.Promise;
+
+  exports['default'] = function (name) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    (0, _qunit.module)(name, {
+      beforeEach: function beforeEach() {
+        this.application = (0, _todoTestsHelpersStartApp['default'])();
+
+        if (options.beforeEach) {
+          return options.beforeEach.apply(this, arguments);
+        }
+      },
+
+      afterEach: function afterEach() {
+        var _this = this;
+
+        var afterEach = options.afterEach && options.afterEach.apply(this, arguments);
+        return Promise.resolve(afterEach).then(function () {
+          return (0, _todoTestsHelpersDestroyApp['default'])(_this.application);
+        });
+      }
+    });
+  };
+});
+define('todo/tests/helpers/module-for-acceptance.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - helpers/module-for-acceptance.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/module-for-acceptance.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/helpers/replace-app-ref', ['exports'], function (exports) {
+  exports['default'] = replaceAppRef;
+  /**
+   * Updates the supplied app adapter's Firebase reference.
+   *
+   * @param  {!Ember.Application} app
+   * @param  {!firebase.database.Reference} ref
+   * @param  {string} [model]  The model, if overriding a model specific adapter
+   */
+
+  function replaceAppRef(app, ref) {
+    var model = arguments.length <= 2 || arguments[2] === undefined ? 'application' : arguments[2];
+
+    app.register('service:firebaseMock', ref, { instantiate: false, singleton: true });
+    app.inject('adapter:firebase', 'firebase', 'service:firebaseMock');
+    app.inject('adapter:' + model, 'firebase', 'service:firebaseMock');
+  }
+});
+define('todo/tests/helpers/replace-firebase-app-service', ['exports'], function (exports) {
+  exports['default'] = replaceFirebaseAppService;
+  /**
+   * Replaces the `firebaseApp` service with your own using injection overrides.
+   *
+   * This is usually not needed in test modules, where you can re-register over
+   * existing names in the registry, but in acceptance tests, some registry/inject
+   * magic is needed.
+   *
+   * @param  {!Ember.Application} app
+   * @param  {!Object} newService
+   */
+
+  function replaceFirebaseAppService(app, newService) {
+    app.register('service:firebaseAppMock', newService, { instantiate: false, singleton: true });
+    app.inject('torii-provider:firebase', 'firebaseApp', 'service:firebaseAppMock');
+    app.inject('torii-adapter:firebase', 'firebaseApp', 'service:firebaseAppMock');
+  }
+});
+define('todo/tests/helpers/resolver', ['exports', 'todo/resolver', 'todo/config/environment'], function (exports, _todoResolver, _todoConfigEnvironment) {
+
+  var resolver = _todoResolver['default'].create();
+
+  resolver.namespace = {
+    modulePrefix: _todoConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _todoConfigEnvironment['default'].podModulePrefix
+  };
+
+  exports['default'] = resolver;
+});
+define('todo/tests/helpers/resolver.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - helpers/resolver.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/resolver.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/helpers/start-app', ['exports', 'ember', 'todo/app', 'todo/config/environment'], function (exports, _ember, _todoApp, _todoConfigEnvironment) {
+  exports['default'] = startApp;
+
+  function startApp(attrs) {
+    var attributes = _ember['default'].merge({}, _todoConfigEnvironment['default'].APP);
+    attributes = _ember['default'].merge(attributes, attrs); // use defaults, but you can override;
+
+    return _ember['default'].run(function () {
+      var application = _todoApp['default'].create(attributes);
+      application.setupForTesting();
+      application.injectTestHelpers();
+      return application;
+    });
+  }
+});
+define('todo/tests/helpers/start-app.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - helpers/start-app.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'helpers/start-app.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/helpers/stub-firebase', ['exports', 'firebase'], function (exports, _firebase) {
+  exports['default'] = stubFirebase;
+
+  /**
+   * When a reference is in offline mode it will not call any callbacks
+   * until it goes online and resyncs. The ref will have already
+   * updated its internal cache with the changed values so we shortcut
+   * the process and call the supplied callbacks immediately (asynchronously).
+   */
+
+  function stubFirebase() {
+    // check for existing stubbing
+    if (!_firebase['default']._unStub) {
+      var originalSet = _firebase['default'].database.Reference.prototype.set;
+      var originalUpdate = _firebase['default'].database.Reference.prototype.update;
+      var originalRemove = _firebase['default'].database.Reference.prototype.remove;
+
+      _firebase['default']._unStub = function () {
+        _firebase['default'].database.Reference.prototype.set = originalSet;
+        _firebase['default'].database.Reference.prototype.update = originalUpdate;
+        _firebase['default'].database.Reference.prototype.remove = originalRemove;
+      };
+
+      _firebase['default'].database.Reference.prototype.set = function (data, cb) {
+        originalSet.call(this, data);
+        if (typeof cb === 'function') {
+          setTimeout(cb, 0);
+        }
+      };
+
+      _firebase['default'].database.Reference.prototype.update = function (data, cb) {
+        originalUpdate.call(this, data);
+        if (typeof cb === 'function') {
+          setTimeout(cb, 0);
+        }
+      };
+
+      _firebase['default'].database.Reference.prototype.remove = function (cb) {
+        originalRemove.call(this);
+        if (typeof cb === 'function') {
+          setTimeout(cb, 0);
+        }
+      };
+    }
+  }
+});
+define('todo/tests/helpers/unstub-firebase', ['exports', 'firebase'], function (exports, _firebase) {
+  exports['default'] = unstubFirebase;
+
+  function unstubFirebase() {
+    if (typeof _firebase['default']._unStub === 'function') {
+      _firebase['default']._unStub();
+      delete _firebase['default']._unStub;
+    }
+  }
+});
+define('todo/tests/models/todo.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - models/todo.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/todo.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/resolver.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - resolver.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/router.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - router.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/routes/todos.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - routes/todos.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/todos.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/routes/todos/edit.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - routes/todos/edit.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/todos/edit.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/routes/todos/new.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - routes/todos/new.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/todos/new.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/test-helper', ['exports', 'todo/tests/helpers/resolver', 'ember-qunit'], function (exports, _todoTestsHelpersResolver, _emberQunit) {
+
+  (0, _emberQunit.setResolver)(_todoTestsHelpersResolver['default']);
+});
+define('todo/tests/test-helper.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - test-helper.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/unit/controllers/todos-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('controller:todos', 'Unit | Controller | todos', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var controller = this.subject();
+    assert.ok(controller);
+  });
+});
+define('todo/tests/unit/controllers/todos-test.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - unit/controllers/todos-test.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/controllers/todos-test.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/unit/controllers/todos/edit-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('controller:todos/edit', 'Unit | Controller | todos/edit', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var controller = this.subject();
+    assert.ok(controller);
+  });
+});
+define('todo/tests/unit/controllers/todos/edit-test.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - unit/controllers/todos/edit-test.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/controllers/todos/edit-test.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/unit/controllers/todos/new-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('controller:todos/new', 'Unit | Controller | todos/new', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var controller = this.subject();
+    assert.ok(controller);
+  });
+});
+define('todo/tests/unit/controllers/todos/new-test.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - unit/controllers/todos/new-test.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/controllers/todos/new-test.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/unit/models/todo-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleForModel)('todo', 'Unit | Model | todo', {
+    // Specify the other units that are required for this test.
+    needs: []
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var model = this.subject();
+    // let store = this.store();
+    assert.ok(!!model);
+  });
+});
+define('todo/tests/unit/models/todo-test.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - unit/models/todo-test.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/models/todo-test.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/unit/routes/todos-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('route:todos', 'Unit | Route | todos', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('todo/tests/unit/routes/todos-test.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - unit/routes/todos-test.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/todos-test.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/unit/routes/todos/edit-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('route:todos/edit', 'Unit | Route | todos/edit', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('todo/tests/unit/routes/todos/edit-test.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - unit/routes/todos/edit-test.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/todos/edit-test.js should pass ESLint.\n');
+  });
+});
+define('todo/tests/unit/routes/todos/new-test', ['exports', 'ember-qunit'], function (exports, _emberQunit) {
+
+  (0, _emberQunit.moduleFor)('route:todos/new', 'Unit | Route | todos/new', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    var route = this.subject();
+    assert.ok(route);
+  });
+});
+define('todo/tests/unit/routes/todos/new-test.lint-test', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('ESLint - unit/routes/todos/new-test.js');
+  QUnit.test('should pass ESLint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/todos/new-test.js should pass ESLint.\n');
+  });
+});
+require('ember-todo/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
